2f56d95cc9e7580873c0d048898e8ffe0243e676
murilo1008 <103895206+murilo1008@users.noreply.github.com>
2024-05-06 09:57:09 -0300

    main fix

--- src/app/(pages)/(home)/goal.tsx
+++ src/app/(pages)/(home)/goal.tsx
@@ -1,6 +1,6 @@
 export default function Goal(){
     return(
-        <div className="flex flex-col w-full items-center mt-40">
+        <div className="flex flex-col w-full items-center md:mt-40 mt-20">
             <h1 className="text-center md:text-6xl text-2xl md:w-2/3 font-extrabold">Nossa meta é <span className="text-primary">aumentar seu alcance</span></h1>
         </div>
     )
--- src/app/(pages)/(home)/main.tsx
+++ src/app/(pages)/(home)/main.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Main(){
     return(
-        <div className="flex flex-col h-lvh justify-center items-center w-full bg-gradient-to-br from-background from-70% to-primary p-5">
+        <div className="flex flex-col h-lvh justify-center items-center w-full bg-gradient-to-br from-background from-70% to-primary p-5 rounded-b-3xl">
             <h1 className="text-center md:text-6xl text-2xl md:w-2/3 font-extrabold">Links curtos que geram <span className="text-primary">vendas, leads e insights</span></h1>
             <p className="text-center md:text-2xl md:w-1/2 mt-5 text-muted-foreground">Saiba exatamente como seus links estão performando e otimize suas campanhas com <span className="text-primary">muito mais eficiência</span>.</p>
             <Button className="mt-10 text-lg">Começar agora</Button>
