4c0261d4af1888e792086c66537678118888c6de
murilo1008 <103895206+murilo1008@users.noreply.github.com>
2024-05-06 10:26:52 -0300

    Fix logo

--- src/app/(pages)/(home)/goal.tsx
+++ src/app/(pages)/(home)/goal.tsx
@@ -1,7 +1,7 @@
 export default function Goal(){
     return(
-        <div className="flex flex-col w-full items-center md:mt-40 mt-20">
-            <h1 className="text-center md:text-6xl text-2xl md:w-2/3 font-extrabold">Nossa meta é <span className="text-primary">aumentar seu alcance</span></h1>
+        <div className="flex flex-col w-full items-center md:mt-40 mt-20 p-5">
+            <h1 className="text-center md:text-6xl text-2xl md:w-2/3 font-extrabold">Crie uma conta gratuitamente</h1>
         </div>
     )
 }
